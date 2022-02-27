2393f2ea0db49c2825a9d9f0693ccf8c9b987546
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2022-02-27 22:13:19 +1300

    add App source file

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+interface Props {
+    
+}
+
+const App: React.FC<Props> = ({
+
+}) => {
+    return <h2>Victor Lian</h2>
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import App from './App'
 import './index.css'
 
 ReactDOM.render(
-    <h1>Hello React 222!</h1>,
+    <App />,
     document.getElementById('app-root'),
 )
