c0a1cd07ae3868215e5c7f63dd424a99deb8f222
DESKTOP-8BK4FU9\victo <victorlian97@gmail.com>
2022-03-07 22:52:43 +1300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { ChakraProvider } from "@chakra-ui/react"
 import Page from "./Page";
+import theme from './theme'
 
 const App = () => {
-    return <ChakraProvider>
+    return <ChakraProvider theme={theme}>
         <Page />
     </ChakraProvider>
 }
